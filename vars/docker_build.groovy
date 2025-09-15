32c169df12222a472294e3b4657c7bd0d93091a2
NAYANZAGADE <119698703+NAYANZAGADE@users.noreply.github.com>
2025-09-15 14:41:47 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,12 @@
+// def call(ProjectName, String ImageTag, String DockerHubUser) {
+//     def imageName = "${DockerHubUser}/${ProjectName}:${ImageTag}"
+//     sh "docker build -t ${imageName} ."
+// }
 def call(ProjectName, String ImageTag, String DockerHubUser) {
-    def imageName = "${DockerHubUser}/${ProjectName}:${ImageTag}"
+    def userLower = DockerHubUser.toLowerCase()
+    def projectLower = ProjectName.toLowerCase()
+    def cleanTag = ImageTag.replaceAll("[^a-zA-Z0-9_.-]", "-").toLowerCase()
+
+    def imageName = "${userLower}/${projectLower}:${cleanTag}"
     sh "docker build -t ${imageName} ."
 }
