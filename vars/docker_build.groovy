b3dfd80c96a157d48f44886cee758dff9d839112
NAYANZAGADE <119698703+NAYANZAGADE@users.noreply.github.com>
2025-09-15 14:39:55 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call(ProjectName, String ImageTag, String DockerHubUser) {
-    def imageName = "${DockerHubUser.toLowerCase()}/${ProjectName.toLowerCase()}:${ImageTag}"
+    def imageName = "${DockerHubUser}/${ProjectName}:${ImageTag}"
     sh "docker build -t ${imageName} ."
 }
