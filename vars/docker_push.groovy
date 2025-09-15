d872e425079cdec4e8a5e7127e08ce20eeb3f95a
NAYANZAGADE <119698703+NAYANZAGADE@users.noreply.github.com>
2025-09-15 15:02:24 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'nayanzagade7', passwordVariable: 'nayanzagade7pass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+      sh "docker login -u ${dockerhubuser} -p ${nayanzagade7pass}"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
